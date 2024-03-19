2c6e5d570bfaa1de324ca68f48e385998fd6ca1a
Yura <svin07@ukr.net>
2024-03-19 16:10:14 +0200

    update home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,15 +37,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `about-piercing-site`,
+        short_name: `piercing`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,61 @@
 import * as React from "react"
-// import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { Link, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <div className="flex ">
-      <h1>
+const IndexPage = ({ data }) => {
+  const { nodes } = data.allMarkdownRemark
+
+  return (
+    <Layout>
+      <h1 className="ml-8 my-4">
         Welcome to <b>Edge-Of-Piercing</b>
       </h1>
-      <StaticImage
-        className="rounded-xl ml-4"
-        src="../images/piercing-3759367_1920-min.jpg"
-        loading="eager"
-        width={256}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt="piercing"
-      />
-    </div>
-  </Layout>
-)
+      <ul className="mx-24 flex flex-col gap-8">
+        {nodes.map(post => {
+          const { category, title, url, image } = post.frontmatter
+          const img = getImage(image)
+          return (
+            <li
+              key={post.id}
+              className="flex gap-8 p-4 items-center border-2 border-solid border-white cursor-pointer hover:scale-105 hover:bg-slate-700 transition-transform"
+            >
+              <GatsbyImage
+                image={img}
+                className="rounded-xl w-24 h-24"
+                alt={title}
+              />
+              <Link to={`/${category}/${url}`}>{title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+}
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          category
+          title
+          url
+          image {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, width: 96, formats: AUTO)
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
