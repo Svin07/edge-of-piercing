add00f6938808aac857695d1f68000c6931a12ed
Svin07 <svin07@ukr.net>
2024-03-20 20:46:08 +0200

    add templates

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,10 @@ import logo from "../images/Piercing_studio_transparent.png"
 
 const Header = ({ siteTitle }) => (
   <header className="bg-slate-100 grid grid-cols-2 justify-items-center text-slate-700 my-auto">
-    <img className="w-36 h-36" src={logo} alt="Logo" />
-    <Link to="/">{siteTitle}</Link>
+    <img className="w-36 h-36 -mt-5" src={logo} alt="Logo" />
+    <nav className="h-4 mt-6">
+      <Link to="/">{siteTitle}</Link>
+    </nav>
   </header>
 )
 
---
+++ src/templates/single-post.js
@@ -0,0 +1,55 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const SinglePost = ({ data }) => {
+  const { html } = data.markdownRemark
+  const { title, url, category, image } = data.markdownRemark.frontmatter
+  const img = getImage(image)
+
+  return (
+    <Layout>
+      <div>
+        <h1>{title}</h1>
+        <p>{`/${category}/${url}`}</p>
+        <div>
+          <GatsbyImage image={img} className="rounded-xl" alt={title} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export const Head = ({ data }) => (
+  <Seo title={data.markdownRemark.frontmatter.title} />
+)
+
+export default SinglePost
+
+export const query = graphql`
+  query PostQuery($url: String) {
+    markdownRemark(frontmatter: { url: { eq: $url } }) {
+      html
+      frontmatter {
+        title
+        url
+        category
+        image {
+          childrenImageSharp {
+            gatsbyImageData(
+              formats: JPG
+              layout: FULL_WIDTH
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
+    }
+  }
+`
